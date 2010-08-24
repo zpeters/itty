2ecea59a461337fcc674a0f697e2d82cfa59aced
Zach Peters <zpeters@gmail.com>
2010-08-23 21:28:07 -0500

    Actually starting to work

--- src/itty.erl
+++ src/itty.erl
@@ -1,82 +1,45 @@
-%%%-------------------------------------------------------------------
-%%% File    : itty.erl
-%%% Author  : Zach Peters <zach@thehelpfulhacker.net>
-%%% Description : An itty bitty web server
-%%%
-%%% Created : 16 Aug 2010 by Zach Peters <zach@thehelpfulhacker.net>
-%%%-------------------------------------------------------------------
 -module(itty).
--export([start/0, stop/0, loop/0, serve/0]).
-
-
-%%% Defines
+-export([start/0]).
 -define(PORT, 5678).
 
-
-%%%-------------------------------------------------------------------
-%%% API
 start() ->
-    logging:log(?MODULE, "Starting..."),
-    Pid = spawn(?MODULE, loop, []),
-    register(itty, Pid),
-    ok.
+    io:format("Initializing server on port ~p...~n", [?PORT]),
+    {ok, ListenSocket} = gen_tcp:listen(?PORT,
+					[list,
+					 {active, false},
+					 {keepalive, true},
+					 {packet, http},
+					 {reuseaddr, true}]),
+    server(ListenSocket).
 
-stop() ->
-    logging:log(?MODULE, "Stopping..."),
-    itty ! stop,
-    unregister(itty),
-    ok.
-
-
-%%%-------------------------------------------------------------------
-%%% Internal
-loop() ->
-    receive
-	stop ->
-	    ok;
-	{get, Path} ->
-	    logging:log(?MODULE, io_lib:format("GET '~s'", [Path])),
-	    logging:log(?MODULE, "Spawing server"),
-	    spawn(?MODULE, serve, []);
-	Any ->
-	    logging:log(?MODULE, io_lib:format("Got signal '~s'", [Any])),
-	    loop()
-    end.
-
-
-serve() ->
-    io:format("Attaching socket to port [~p]~n", [?PORT]),
-    {ok, ListenSocket} = gen_tcp:listen(?PORT, 
-					[binary, {packet, http},
-					 {active, false}]),
-    {ok, RecvSocket} = gen_tcp:accept(ListenSocket),
-    {ok, {http_request, HttpMethod, HttpUri, HttpVersion}} = gen_tcp:recv(RecvSocket, 0),
+server(ListenSocket) ->
+    handle(ListenSocket),
+    server(ListenSocket).
+   
+handle(ListenSocket) ->
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    {ok, {http_request, HttpMethod, HttpUri, _HttpVersion}} = gen_tcp:recv(Socket, 0),
     io:format("Processing request...~n"),
     io:format("HTTP Method: ~p~n", [HttpMethod]),
     io:format("HTTP URI: ~p~n", [HttpUri]),
-    io:format("HTTP Version: ~p~n", [HttpVersion]),
-    io:format("Sending Response...~n"),
-    Body = io_lib:format("<html><head><title>hello</title></head><body><h1>Hello, world</h1><hr><b>~p</b></body></html>", [erlang:date()]),
+    Body = "<html><head><title>Hello</title></head><body><h1>Hello, world</h1></body></html>",
     {Header, NewBody} = gen_header(200, Body),
-    Packet = string:concat(Header,NewBody),
-    io:format("Packet: ~p~n", [Packet]),
-    ok = gen_tcp:send(RecvSocket, Packet),
-    ok = gen_tcp:close(RecvSocket),
-    ok = gen_tcp:close(ListenSocket),
-    ok.
+    Packet = string:concat(Header, NewBody),
+    ok = gen_tcp:send(Socket, Packet),
+    ok = gen_tcp:close(Socket).
 
 gen_header(Response, Body) ->
     Version = "HTTP/1.0",
-    case Response of 
-	200 ->
-	    ResponseCode = "200 OK",
-	    NewBody = Body;
-	404 ->
-	    ResponseCode = "404 Not Found",
-	    NewBody = "<h1>Not Found</h1>";
-	_Any ->
-	    ResponseCode = "500 Intern Server Error FUUUUUuuuu",
-	    NewBody = "<h1>FFFFUUUUuuuuu</h1>"
+    case Response of
+        200 ->
+            ResponseCode = "200 OK",
+	    NewBody = Body ++ "<hr>" ++ gen_time();
+        404 ->
+            ResponseCode = "404 Not Found",
+            NewBody = "<h1>Not Found</h1>";
+        _Any ->
+            ResponseCode = "500 Intern Server Error FUUUUUuuuu",
+            NewBody = "<h1>FFFFUUUUuuuuu</h1>"
     end,
     HttpResponse = io_lib:format("~s ~s\r\n", [Version, ResponseCode]),
     Server = "Server: itty/0.1\r\n",
@@ -84,8 +47,9 @@ gen_header(Response, Body) ->
     ContentLength = io_lib:format("Content-Length: ~p\r\n", [string:len(NewBody)]),
     HttpHeader = io_lib:format("~s~s~s~s\r\n", [HttpResponse, Server, ContentType, ContentLength]),
     {HttpHeader, NewBody}.
+
+gen_time() ->
+    {{Year, Month, Day}, {Hour, Min, Seconds}} = erlang:localtime(),
+    Date = io_lib:format("~p~p~p ~p:~p:~p~n", [Year, Month, Day, Hour, Min, Seconds]),
+    Date.
     
-
-
-
-			  
