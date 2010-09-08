27890c9379ad08cb3f21e359f15aa48a44068bb8
Zach Peters <zpeters@gmail.com>
2010-09-07 21:27:14 -0500

    Switched to using records, added some more todos

--- src/itty.erl
+++ src/itty.erl
@@ -1,5 +1,8 @@
 -module(itty).
--export([start/0, handler/1, gen_header/1, gen_header/2, gen_time/0, time_zone/0, time_zone/1]).
+-export([start/0, handler/1, 
+	 gen_header/1, gen_header/2, gen_time/0, 
+	 time_zone/0, time_zone/1,
+	gen_http_request_record/2]).
 
 -define(TCP_OPTS, [list,
 		   {active, false},
@@ -9,10 +12,22 @@
 
 -define(BODY_404, "<html><head><title>404 Not Found</title></head><body>404 - LOL</body></html>").
 -define(BODY_500, "<html><head><title>500 FUUUUuuuu</title></head><body>500 - FUUUUuuuu<br>Unknown error</body></html>").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% records
+-record(http_request,
+	{requestor_ip,
+	 http_path,
+	 http_method,
+	 http_ver_maj,
+	 http_ver_min,
+	 client_identity="-",
+	 client_username="-"
+	 }).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% API
 start() ->
-    io:format("Initializing server...~n"),
     io:format("Port: ~p~n", [?PORT]),
     io:format("TCP Options: ~p~n", [?TCP_OPTS]),
     do_listen(?PORT, ?TCP_OPTS, handler).
@@ -25,7 +40,6 @@ do_listen(Port, Opts, Handler) ->
 	{ ok, ListeningSocket } ->
 	    listen_loop(ListeningSocket, Handler);
 	{ error, E } ->
-	    io:format("Error: ~p~n", [E]),
 	    { error, E }
     end.
 
@@ -35,14 +49,64 @@ listen_loop(ListeningSocket, Handler) ->
 	    spawn(node(), ?MODULE, Handler, [ConnectedSocket]),
 	    listen_loop(ListeningSocket, Handler);
 	{ error, E } ->
-	    io:format("Error: ~p~n", [E]),
 	    { error, E }
     end.
 	    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Handler   
 
+gen_http_request_record(HttpRequest, RequestorIP) ->
+    {http_request, HttpMethod, HttpUri, HttpVersion} = HttpRequest,
+    {MajVer, MinVer} = HttpVersion,
+    {_HttpReq, HttpPath} = HttpUri,
+    RequestRecord = #http_request{
+      requestor_ip=RequestorIP,
+      http_path=HttpPath,
+      http_method=HttpMethod,
+      http_ver_maj=MajVer,
+      http_ver_min=MinVer
+     },
+    RequestRecord.
 
+handler(ConnectedSocket) ->
+    {ok, HttpRequest} = gen_tcp:recv(ConnectedSocket, 0),
+    {ok, {RequestorIP, _ClientPort}} = inet:peername(ConnectedSocket),
+    RequestRecord = gen_http_request_record(HttpRequest, RequestorIP),
+    case serve_request(RequestRecord) of
+	{ok, {200, Body}} ->
+	    Packet = gen_packet(200, Body);
+	{error, {404, not_found}} ->
+	    Packet = gen_packet({error, {404, not_found}}, ?BODY_404);
+	{ error, _Any } ->
+	    Packet = gen_packet({error, {500, not_found}}, ?BODY_500)
+    end,
+    gen_tcp:send(ConnectedSocket, Packet),
+    gen_tcp:close(ConnectedSocket).
+
+serve_request(RequestRecord) ->
+    case RequestRecord#http_request.http_path of 
+	[] ->
+	    Request = ?DOCROOT ++ "/index.html";
+	"/" ->
+	    Request = ?DOCROOT ++ "/index.html";
+	_Any ->
+	    Request = ?DOCROOT ++ RequestRecord#http_request.http_path
+    end,
+    case file:read_file(Request) of
+	{ok, File} ->
+	    FileContents = binary_to_list(File),
+	    BodyLength = string:len(FileContents),
+	    log_request(RequestRecord, 200, BodyLength),
+	    {ok, {200, FileContents}};
+	_ ->
+	    BodyLength = string:len(?BODY_404),
+	    log_request(RequestRecord, 404, BodyLength),
+	    {error, {404, not_found}}
+    end.
+
+	    	    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% HTTP Utility Functions
 gen_header({error, {500, Error}}) ->
     Body = "<html><head><title>500 FUUUUuuuu</title></head><body>500 - FUUUUuuuu<br>" ++ Error ++ "</body></html>",
     gen_header(500, Body);
@@ -73,66 +137,27 @@ gen_packet(Response, Body) ->
     Header = gen_header(Response, Body),
     string:concat(Header, Body).
 
-handler(ConnectedSocket) ->
-    case gen_tcp:recv(ConnectedSocket, 0) of
-	{ok, {http_request, HttpMethod, HttpUri, HttpVersion}} ->
-	    {_HttpReq, HttpPath} = HttpUri,
-	    {ok, {RequestorIP, _ClientPort}} = inet:peername(ConnectedSocket),
-	    case serve_request(RequestorIP, HttpPath, HttpMethod, HttpVersion) of
-		{ok, {200, Body}} ->
-		    Packet = gen_packet(200, Body);
-		{error, {404, not_found}} ->
-		    Packet = gen_packet({error, {404, not_found}}, ?BODY_404);
-		{error, Error} ->
-		    Packet = gen_packet({error, {500, Error}}, ?BODY_500)
-	    end,
-	    gen_tcp:send(ConnectedSocket, Packet),
-	    gen_tcp:close(ConnectedSocket);
-	Any ->
-	    io:format("I got request: ~p~n", [Any])
-    end.
 
-serve_request(RequestorIP, HttpPath, HttpMethod, HttpVersion) ->
-    case HttpPath of 
-	[] ->
-	    Request = ?DOCROOT ++ "/index.html";
-	"/" ->
-	    Request = ?DOCROOT ++ "/index.html";
-	_Any ->
-	    Request = ?DOCROOT ++ HttpPath
-    end,
-    case file:read_file(Request) of
-	{ok, File} ->
-	    FileContents = binary_to_list(File),
-	    BodyLength = string:len(FileContents),
-	    log_request(RequestorIP, "-", "-", HttpMethod, HttpPath, HttpVersion, 200, BodyLength),
-	    {ok, {200, FileContents}};
-	_ ->
-	    BodyLength = string:len(?BODY_404),
-	    log_request(RequestorIP, "-", "-", HttpMethod, HttpPath, HttpVersion, 404, BodyLength),
-	    {error, {404, not_found}}
-    end.
-
-	    	    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Utility Functions
-log_request(RequestorIP, ClientIdentity, ClientUsername,
-	   HttpMethod, HttpPath,
-	   HttpVersion,
-	   StatusCode, BodyLength) ->
-	    {Year, Month, Day} = date(),
-	    {Hour, Minute, Second} = time(),
-	    TimeZone = time_zone(),
-	    {MajorVersion, MinorVersion} = HttpVersion,    
-    io:format("~p ~s ~s [~p-~p-~p ~p:~p:~p ~s] \"~s ~s HTTP/~p.~p\" ~p ~p~n", 
-	      [RequestorIP, ClientIdentity, ClientUsername,
+log_request(RequestRecord, StatusCode, BodyLength) ->
+    {Year, Month, Day} = date(),
+    {Hour, Minute, Second} = time(),
+    io:format("~p ~p ~p ~p [~p-~p-~p ~p:~p:~p ~s] \"~p ~p HTTP/~p.~p\" ~p ~p~n", 
+	      [
+	       RequestRecord#http_request.requestor_ip,
+	       RequestRecord#http_request.client_identity,
+	       RequestRecord#http_request.client_username,
+	       RequestRecord#http_request.requestor_ip,
 	       Year, Month, Day,
-	       Hour, Minute, Second, 
-	       TimeZone,
-	       HttpMethod, HttpPath,
-	       MajorVersion, MinorVersion,
+	       Hour, Minute, Second,
+	       time_zone(),
+	       RequestRecord#http_request.http_method,
+	       RequestRecord#http_request.http_path,
+	       RequestRecord#http_request.http_ver_maj,
+	       RequestRecord#http_request.http_ver_min,
 	       StatusCode, BodyLength]).
-	
+
 gen_time() ->
     {{Year, Month, Day}, {Hour, Min, Seconds}} = erlang:localtime(),
     Date = io_lib:format("~p~p~p ~p:~p:~p~n", [Year, Month, Day, Hour, Min, Seconds]),
